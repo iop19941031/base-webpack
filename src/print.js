09b6a55145981d727f603017bf5eb692ad6de768
iop19941031 <402284347@qq.com>
2020-05-08 17:33:03 +0800

    防止重复

--- src/print.js
+++ src/print.js
@@ -1,5 +1,11 @@
+import _ from 'lodash';
+
 function printMe() {
     console.log('I get called from print.js!');
+
+    console.log(
+        _.join(['Another', 'module', 'loaded!'], ' ')
+    );
 }
 
 export {
--- webpack.prod.js
+++ webpack.prod.js
@@ -4,6 +4,13 @@ const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
     devtool: 'source-map',
+    optimization: {
+        //抽取公共的dm
+        splitChunks: {
+            name: "commons",
+            chunks: 'all'
+        }
+    },
     plugins: [
         new UglifyJSPlugin()
     ]
