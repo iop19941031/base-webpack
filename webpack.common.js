d2dcb5b9c6301af321a43dae1de03d4130e5f694
liang <402284347@qq.com>
2020-07-21 18:02:06 +0800

    webpack插件thread-loader 加快构建速度

--- webpack.common.js
+++ webpack.common.js
@@ -73,6 +73,39 @@ module.exports = {
                 use: [
                     'xml-loader'
                 ]
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: "thread-loader",
+                        options: {
+                            // 产生的 worker 的数量，默认是 cpu 的核心数
+                            workers: 4,
+                            // 一个 worker 进程中并行执行工作的数量,默认为20
+                            workerParallelJobs: 50,
+                            // Allow to respawn a dead worker pool
+                            // respawning slows down the entire compilation
+                            // and should be set to false for development
+                            poolRespawn: false,
+
+                            // 闲置时定时删除 worker 进程
+                            // 默认为 500ms
+                            // 可以设置为无穷大， 这样在监视模式(--watch)下可以保持 worker 持续存在
+                            poolTimeout: 2000,
+
+                            // 池(pool)分配给 worker 的工作数量
+                            // 默认为 200
+                            // 降低这个数值会降低总体的效率，但是会提升工作分布更均一
+                            poolParallelJobs: 50,
+
+                            // 池(pool)的名称
+                            // 可以修改名称来创建其余选项都一样的池(pool)
+                            name: "my-pool"
+                        }
+                    },
+                ]
             }
         ]
     },
